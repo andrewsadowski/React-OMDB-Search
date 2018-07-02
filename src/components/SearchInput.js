d6f8194810a2999c56d849f5fe6002a085fdc87a
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-07-01 18:42:44 -0700

    updated SearchInput component to use refs

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,44 +1,78 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class SearchInput extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      search: ''
-    };
+    this.searchRef = React.createRef();
   }
 
-  submitHandler(e) {
+  getSearch = e => {
     e.preventDefault();
-    this.props.handlerFromParent(this.state.search);
-    this.setState({
-      search: ''
-    });
-  }
 
-  handleChange(e) {
-    e.preventDefault();
-    this.setState({
-      search: e.target.value
-    });
-  }
+    const search = this.searchRef.current.value;
+    console.log(search);
+    this.props.handleSearch(search);
+  };
 
   render() {
     return (
-      <div>
-        <form onSubmit={this.submitHandler.bind(this)}>
-          <input
-            type="text"
-            onChange={this.handleChange.bind(this)}
-            value={this.state.search}
-            placeholder="Search for a movie..."
-          />
-          <input type="submit" />
-        </form>
-        {this.state.search}
-      </div>
+      <form onSubmit={this.getSearch.bind(this)}>
+        <input
+          type="text"
+          name="movieSearch"
+          ref={this.searchRef}
+          placeholder="Search for a movie..."
+        />
+        <input type="submit" />
+      </form>
     );
   }
 }
 
 export default SearchInput;
+
+
+// import React, { Component } from 'react';
+
+// class SearchInput extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       search: ''
+//     };
+//   }
+
+//   submitHandler(e) {
+//     e.preventDefault();
+//     this.props.handlerFromParent(this.state.search);
+//     this.setState({
+//       search: ''
+//     });
+//   }
+
+//   handleChange(e) {
+//     e.preventDefault();
+//     this.setState({
+//       search: e.target.value
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <form onSubmit={this.submitHandler.bind(this)}>
+//           <input
+//             type="text"
+//             onChange={this.handleChange.bind(this)}
+//             value={this.state.search}
+//             placeholder="Search for a movie..."
+//           />
+//           <input type="submit" />
+//         </form>
+//         {this.state.search}
+//       </div>
+//     );
+//   }
+// }
+
+// export default SearchInput;
