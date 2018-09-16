9534588d14afd6ff5f77d291107e80c87dda2c36
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-09-16 15:16:44 -0700

    added y axis transtition for errorcard

--- src/components/ErrorCard.js
+++ src/components/ErrorCard.js
@@ -5,7 +5,11 @@ export default class ErrorCard extends Component {
   render() {
     return (
       <div>
-        <Transition from={{ opacity: 0 }} enter={{ opacity: 1 }}>
+        <Transition
+          from={{ opacity: 0, y: 200 }}
+          enter={{ opacity: 1, y: 0 }}
+          leave={{ opacity: 0, y: 200 }}
+        >
           {ErrCard}
         </Transition>
       </div>
@@ -16,7 +20,9 @@ export default class ErrorCard extends Component {
 const ErrCard = styles => (
   <div
     style={{
-      opacity: styles.opacity
+      opacity: styles.opacity,
+      transform: `translate3d(0, ${styles.y}px, 0)`,
+      ...styles
     }}
   >
     <h1>Try searching for a different movie...</h1>
